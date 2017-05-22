b370c05b3302b423e89eb12608a40666cca83a0a
Huỳnh Lê Minh Trí <hlmtri@mac09.local>
2017-05-22 16:16:25 +0700

    commit

---
+++ MomoApp/DesignButton.swift
@@ -0,0 +1,36 @@
+//
+//  DesignButton.swift
+//  MomoApp
+//
+//  Created by Huỳnh Lê Minh Trí on 5/22/17.
+//  Copyright © 2017 cusc. All rights reserved.
+//
+
+//import Cocoa
+import UIKit
+@IBDesignable
+
+class DesignButton: UIButton {
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+}
---
+++ MomoApp/DesignImageView.swift
@@ -0,0 +1,35 @@
+//
+//  DesignImageView.swift
+//  MomoApp
+//
+//  Created by Huỳnh Lê Minh Trí on 5/22/17.
+//  Copyright © 2017 cusc. All rights reserved.
+//
+
+//import Cocoa
+import UIKit
+
+@IBDesignable
+class DesignImageView: UIImageView {
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }}
